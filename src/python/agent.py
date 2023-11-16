e8fbee61655ce7f5cb162da71a0e82d46f4992b6
d7ryLinux <yty108102@gmail.com>
2023-11-15 17:13:03 -0800

    L1 cache working

--- src/python/agent.py
+++ src/python/agent.py
@@ -1,12 +1,12 @@
-import json
 import os
+from typing import Optional
 import openai
-from memory import Memory
+from memory import Memory, Context
 # Assuming a placeholder for OpenAI's GPT API interaction
 # from openai import GPT
 
 class Agent:
-    def __init__(self, agent_id: str=""):
+    def __init__(self, agent_id: str="", agent_summary: str=""):
         
         memory_dict: dict|None = None
         if agent_id != "":
@@ -15,7 +15,7 @@ class Agent:
             raise NotImplementedError("De-serialization not implemented yet")
 
         # intiailize memory
-        self.memory: Memory = Memory(memory_dict)
+        self.memory: Memory = Memory(memory_dict, summary=agent_summary)
 
         # Initialize OpenAI
         openai_key: str|None = os.environ.get("OPENAI_API_KEY")
@@ -23,62 +23,51 @@ class Agent:
             raise Exception("OPENAI_API_KEY environment variable not set.")
         openai.api_key = openai_key
 
-    def ask(self, question: str) -> str:
+    def ask(self, question: str) -> Optional[str]:
         # Recall relevant information from memory
-        recalled_info = self.__recall(question)
         
-        # Construct the context for GPT
-        context = {
-            "L1_memory": self.__L1_cache,
-            "L2_memory": self.__L2_cache,
-            "L3_memory": recalled_info  # Placeholder for L3 info
-        }
-        
+        context: Context = self.memory.get_memory(question)
         # Placeholder for querying OpenAI's GPT API
-        response: str = self.__query_gpt_api(question, context)
-        
+        response: str|None = self.__query_gpt_api(question, context)
+        # TODO: paralllize memoirzation 
         # Update memory after the conversation
-        self.__memorize(question, response)
-        
+        if response is not None: 
+            self.memory.memorize(question, response)
         return response
 
-    def __recall(self, question):
-        # Placeholder for logic to check L3 cache necessity and retrieval
-        # For now, we return an empty object or relevant info if applicable
-        return {}
 
-    def __memorize(self, question, response):
-        # Update L1 cache with the recent conversation
-        self.__L1_cache.append({"question": question, "response": response})
-        
-        # Implement logic to condense L1 cache to L2, and manage overflow
-        
-        # Serialize the L1 and L2 cache state if needed
-        # self.__serialize_memory()
 
-    def __query_gpt_api(self, question, context) -> str:
+    def __query_gpt_api(self, question, context: Context) -> Optional[str]:
         # Placeholder for querying OpenAI's GPT API
         # response = GPT.query(question, context)
         client = openai.OpenAI()
         messages: list = [
+                    {
+                    "role" : "system",
+                    "content" : context.summary
+                    }
                 ]
+        # get things from l2 cache in
+        
         # append everything from l1 cache
+        for conversation in context.most_recent_conversations:
+            messages.append({"role" : "user", "content":conversation[0]})
+            messages.append({"role" : "assistant", "content": conversation[1]})
+
+        # append question
+        messages.append({"role": "user", "content" : question})
+       
         
         completion = client.chat.completions.create(
                 model="gpt-3.5-turbo-1106",
                 messages = messages
                 )
-
-    def __serialize_memory(self):
-        # Serialize the L1 and L2 caches to JSON
-        # This is a placeholder for the actual serialization logic
-        serialized_data = json.dumps({
-            "L1_cache": self.__L1_cache,
-            "L2_cache": self.__L2_cache
-        })
-        return serialized_data
+        print(messages)
+        return completion.choices[0].message.content
 
 # Example usage
-agent = Agent()
-response = agent.ask("What's the weather like today?")
+agent = Agent(agent_summary="You're a very funny person who laughs and makes fun of everything I say")
+response = agent.ask("Hello, my name is dtry")
+print(response)
+response = agent.ask("What is my name?")
 print(response)
--- src/python/memory.py
+++ src/python/memory.py
@@ -1,28 +1,40 @@
 import os
 
+class Context:
+    def __init__(self, l1_memory: 'list[tuple[str, str]]', l2_memory: 'list[str]', l3_memory: 'list[str]', summary: str):
+        self.most_recent_conversations: list[tuple[str, str]] = l1_memory
+        self.l2_memory: list[str] = l2_memory
+        self.l3_memory: list[str] = l3_memory
+        self.summary: str = summary
 
+
+        
 class Memory:
-    def __init__(self, memory_dict: dict|None = None):
+    def __init__(self, memory_dict: 'dict|None' = None, summary: str = ""):
         if memory_dict is not None:
             #TODO: implement de-serialziation
             raise NotImplementedError("De-serialization not implemented yet")
         
         self.__L1_cache: list = []  # List to store recent conversations
         self.__L2_cache: list = []  # List to store synopses of conversations
+        self.__summary: str = summary
 
 
     def memorize(self, user_input: str, agent_input: str):
         self.__L1_cache.append(
-                {"user" : user_input, "agent" : agent_input}
+                (user_input, agent_input)
                 )
+        
 
     def __check_cache_overflow(self):
         #TODO: implement cache overflow check, and flush cache
         raise NotImplementedError("Cache overflow check not implemented yet")
 
-    def __get_relevant_l3_cache(self, user_input: str) -> list[str]:
+    def __get_relevant_l3_cache(self, user_input: str) -> 'list[str]':
         #TODO: implement L3 cache fetching
         raise NotImplementedError("L3 cache not implemented yet!")
 
-    def get_memory(self, user_input: str) -> tuple[list[str], list[str], list[str]]:
-        return (self.__L1_cache, self.__L2_cache, self.__get_relevant_l3_cache(user_input))
+    def get_memory(self, user_input: str) -> Context:
+        #l3_memory: list[str] = self.__get_relevant_l3_cache(user_input)
+        l3_memory = []
+        return Context(self.__L1_cache, self.__L2_cache, l3_memory, self.__summary)
